89794eaefbf5a327172d6778cc5a60f8fdac112f
lesley_w <lrwesley92@gmail.com>
2021-07-17 21:11:36 -0500

    Added global variables to javascript

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Selects Bill Input Section
+
+const billAmount = document.querySelector(".bill-input-div input");
+
+//Selects Percentage Buttons
+
+const percent5 = document.querySelector(".percent5");
+
+const percent10 = document.querySelector(".percent10");
+
+const percent15 = document.querySelector(".percent15");
+
+const percent25 = document.querySelector(".percent25");
+
+const percent50 = document.querySelector(".percent50");
+
+//Selects Custom Tip input
+
+const customTip = document.querySelector("#custom-tip");
+
+//Selects Number of People input
+
+const numPeople = document.querySelector("#people-amount");
+
+//Selects Tip Amount Output
+
+const tipOutput = document.querySelector(".tip-output-half span");
+
+//Selects Total Amount Output
+
+const totalAmountOutput = document.querySelector(".total-output-half span");
+
+//Selects Reset Button
+
+const resetButton = document.querySelector(".reset-button");
+
+
+//===========================================================
